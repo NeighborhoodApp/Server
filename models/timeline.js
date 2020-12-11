3b2668763efe054e4c304a3c817b529f5274aa42
samudrareggi <samudrareggi1@gmail.com>
2020-12-11 10:57:56 +0700

    create model timeline

---
+++ models/timeline.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Timeline extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // Timeline.belongsTo(models.User)
+    }
+  };
+  Timeline.init({
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Please enter timeline description'
+        }
+      }
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    privacy: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Please enter timeline privacy'
+        }
+      }
+    },
+    UserId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+  }, {
+    sequelize,
+    modelName: 'Timeline',
+  });
+  return Timeline;
+};
