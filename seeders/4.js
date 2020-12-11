b751aa0f5505e8348cd93815164c30320b0117a7
Abraham Purnomo <abraham-sys@github.com>
2020-12-12 00:09:35 +0700

    changed seeder contents

--- seeders/4.js
+++ seeders/4.js
@@ -4,7 +4,6 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("Complexes", [
       {
-        id: 1,
         name: "Real Estet 1 Komplek A1",
         RealEstateId: 1,
         status: "Active",
@@ -12,7 +11,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 2,
         name: "Real Estet 1 Komplek B1",
         RealEstateId: 1,
         status: "Active",
@@ -20,7 +18,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 3,
         name: "Real Estet 1 Komplek C1",
         RealEstateId: 1,
         status: "Inactive",
@@ -28,7 +25,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 4,
         name: "Real Estet 2 Komplek A2",
         RealEstateId: 2,
         status: "Active",
@@ -36,7 +32,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 5,
         name: "Real Estet 2 Komplek B2",
         RealEstateId: 2,
         status: "Active",
@@ -44,7 +39,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 6,
         name: "Real Estet 3 Komplek A3",
         RealEstateId: 3,
         status: "Inactive",
@@ -52,7 +46,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 7,
         name: "Real Estet 3 Komplek B3",
         RealEstateId: 3,
         status: "Inactive",
@@ -60,7 +53,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 8,
         name: "Real Estet 4 Komplek A4",
         RealEstateId: 4,
         status: "Inactive",
@@ -68,7 +60,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 9,
         name: "Real Estet 4 Komplek B4",
         RealEstateId: 4,
         status: "Inactive",
@@ -76,7 +67,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 10,
         name: "Real Estet 5 Komplek A5",
         RealEstateId: 5,
         status: "Inactive",
@@ -84,7 +74,6 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        id: 11,
         name: "Real Estet 5 Komplek B5",
         RealEstateId: 5,
         status: "Inactive",
