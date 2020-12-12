f3d9a916ac7551406a1cb7c1ca7f0a71f2f08d88
rpratama94 <rpratama.sky@gmail.com>
2020-12-12 08:42:09 +0700

    Fix conflict router.js

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,7 @@ const comment = require('./comment')
 const userRouter = require("./userRouter.js");
 const developerRouter = require("./developerRouter.js");
 const roleRouter = require("./roleRouter.js");
-<<<<<<< HEAD
-const realEstateRouter = require("./realEstateRouter");
-=======
 const realEstateRouter = require("./realEstateRouter.js");
->>>>>>> 26915655b55de1e26ed816f3282800c7f0021280
 const complexRouter = require("./complexRouter.js");
 
 router.use('/category', category)
