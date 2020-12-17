5dd5a0c78fe02a5ac058e7d3faf23ccf91e8fc07
rpratama94 <rpratama.sky@gmail.com>
2020-12-17 14:50:46 +0700

    Testing Jest

--- controllers/TimelineController.js
+++ controllers/TimelineController.js
@@ -1,118 +1,124 @@
-const { Timeline, User, RealEstate, Comment } = require('../models')
-const { calculateDistance } = require('../helpers/helper')
+const { Timeline, User, RealEstate, Comment, Complex } = require("../models");
+const { calculateDistance } = require("../helpers/helper");
 
 class TimelineController {
   static async find(req, res, next) {
-    const { coordinate } = req.headers
+    const { coordinate } = req.headers;
+    console.log("tess");
     try {
       const timeline = await Timeline.findAll({
         include: [
           {
             model: User,
             include: [
-              {
-                model: RealEstate,
-                required: false
-              }
+              RealEstate,
+              Complex,
             ],
           },
-          Comment
+          {
+            model: Comment,
+            order: [["updatedAt", "DESC"]],
+          },
         ],
-      })
+        order: [["updatedAt", "DESC"]],
+      });
       if (!timeline.length) {
-        throw { msg: 'Timeline not found', status: 404 }
+        throw { msg: "Timeline not found", status: 404 };
       }
-      const userCoordinat = coordinate.replace(/\s/g, "").split(',')
-      const result = calculateDistance(userCoordinat, timeline)
-      res.status(200).json(result)
+      const userCoordinat = coordinate.replace(/\s/g, "").split(",");
+      const result = calculateDistance(userCoordinat, timeline);
+      res.status(200).json(result);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async findById(req, res, next) {
-    const id = req.params.id
+    const id = req.params.id;
     try {
       const timeline = await Timeline.findOne({
         where: {
-          id
+          id,
         },
         include: [
           {
             model: Comment,
+            order: [["updatedAt", "DESC"]],
             include: [
               {
                 model: User,
-                required: false
-              }
+                required: false,
+              },
             ],
           },
+          User,
         ],
-      })
+      });
       if (!timeline) {
-        throw { msg: 'Timeline not found', status: 404 }
+        throw { msg: "Timeline not found", status: 404 };
       }
-      res.status(200).json(timeline)
+      res.status(200).json(timeline);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async create(req, res, next) {
+    console.log(req.body.image);
     const payload = {
       description: req.body.description,
       image: req.body.image,
       privacy: req.body.privacy,
-      UserId: req.loggedIn.id
-    }
+      UserId: req.loggedIn.id,
+    };
     try {
-      const timeline = await Timeline.create(payload)
-      res.status(201).json(timeline)
+      const timeline = await Timeline.create(payload);
+      res.status(201).json(timeline);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async update(req, res, next) {
-    const id = req.params.id
+    const id = req.params.id;
     const payload = {
       description: req.body.description,
       image: req.body.image,
       privacy: req.body.privacy,
-      UserId: req.loggedIn.id
-    }
+      UserId: req.loggedIn.id,
+    };
     try {
       const timeline = await Timeline.update(payload, {
         where: {
-          id
+          id,
         },
-        returning: true
-      })
+        returning: true,
+      });
       if (!timeline[1].length) {
-        throw { msg: 'Timeline not found', status: 404 }
+        throw { msg: "Timeline not found", status: 404 };
       }
-      res.status(200).json(timeline[1][0])
+      res.status(200).json(timeline[1][0]);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async delete(req, res, next) {
-    const id = req.params.id
+    const id = req.params.id;
     try {
       const timeline = await Timeline.destroy({
         where: {
-          id
-        }
-      })
+          id,
+        },
+      });
       if (!timeline) {
-        throw { msg: 'Timeline not found', status: 404 }
+        throw { msg: "Timeline not found", status: 404 };
       }
-      res.status(200).json('Successful deleted timeline')
+      res.status(200).json("Successful deleted timeline");
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
 
-module.exports = TimelineController
+module.exports = TimelineController;
