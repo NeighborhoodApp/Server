049d91b37427ac986d2ae2913d06dbcdfda81b2f
samudrareggi <samudrareggi1@gmail.com>
2020-12-20 22:14:13 +0700

    change order comment

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -4,7 +4,7 @@ const storage = unggah.s3({
   endpoint: 's3-ap-southeast-1.amazonaws.com',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  bucketName: 'tetonggo-h8',
+  bucketName: 'tetonggo-h8-new',
   rename: (req, file) => {
     console.log(file)
     return `${Date.now()}-${file.originalname}`
